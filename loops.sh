c8a586c0a6549f33bde4c293c85ecf7e21c4121f
shahbaz4783 <shahbaz4783@gmail.com>
2023-11-07 01:28:41 +0530

    loops

---
+++ loops.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# NAMES="Alex Henry Kevin"
+
+# for PEOPLES in $NAMES
+#     do
+#         echo Hello $PEOPLES
+# done
+
+
+# Rename file by for loop
+
+FILES=$(ls *.txt)
+RENAME="new"
+
+for FILE in $FILES
+    do
+        echo "Renaming: $FILE to renamed-$FILE"
+        mv $FILE $RENAME-$FILE
+done
+
+
+# Deleting file by for loop
+
+FILES=$(ls *.txt)
+
+for FILE in $FILES
+    do
+        echo "DELETING: $FILE"
+        rm $FILE
+done
